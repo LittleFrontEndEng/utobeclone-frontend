0c792cb2dad93ead1a4bb238baffe63af5fea650
liumangtu <1312483228@qq.com>
2023-09-19 13:45:36 +0800

    feat: add signup api

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './style.css'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import 'element-plus/es/components/message/style/css'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,26 +5,30 @@
       <ul class="errors" v-if="errors">
         <li>{{errors}}</li>
       </ul>
+      <input v-if="isSingUp" v-model="user.username" type="username" placeholder="username" />
       <input v-model="user.email" type="email" placeholder="email" />
       <input v-model="user.password" type="password" placeholder="password" />
       <div class="action input-group">
-        <span class="pointer">Sign Up</span>
+        <span class="pointer" @click="enjoinSignUp">Sign Up</span>
         <!-- <button>Login</button> -->
-        <el-button type="primary" :loading="btnLoading" @click="handleSubmit">Login</el-button>
+        <el-button type="primary" :loading="btnLoading" @click="handleSubmit">{{ isSingUp ? "signup" : 'Login' }}</el-button>
       </div>
     </form>
   </div>
 </template>
 <script setup>
-import { login } from '@/api/user';
+import { login, signup } from '@/api/user';
 import { reactive, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { ElMessage } from 'element-plus'
 import { useStore } from 'vuex'
 const user = reactive({
-  email: "d.wthnnyteq@myrs.gw",
-  password: "123456",
+  username: '',
+  email: "",
+  password: "",
 })
 const errors = ref('');
+const isSingUp = ref(false);
 const btnLoading = ref(false);
 const router = useRouter();
 const route = useRoute();
@@ -32,17 +36,44 @@ const store = useStore();
 const handleSubmit = async () => {
   errors.value = ''
   btnLoading.value = true;
-  try {
-    const result = await login(user)
-    store.commit('addUserInfo', result.user)
-    const redirect = route.query.redirect || '/'
-    localStorage.setItem("userInfo", JSON.stringify(result.user))
-    router.push(redirect)
-  } catch (error) {
-    errors.value = error.error;
-  } finally {
-    btnLoading.value = false;
+  if (!isSingUp.value) {
+
+    try {
+      const result = await login({
+        email: user.email,
+        password: user.password
+      })
+      store.commit('addUserInfo', result.user)
+      const redirect = route.query.redirect || '/'
+      localStorage.setItem("userInfo", JSON.stringify(result.user))
+      router.push(redirect)
+    } catch (error) {
+      errors.value = error.error;
+    } finally {
+      btnLoading.value = false;
+    }
+  } else {
+    try {
+      const result = await signup(user)
+      if (result.success) {
+        ElMessage({
+          message: '注册成功',
+          type: 'success',
+        })
+        user.password = '';
+        user.username = '';
+        isSingUp.value = false;
+      }
+    } catch (error) {
+      errors.value = error.error;
+    } finally {
+      btnLoading.value = false;
+    }
   }
+}
+
+const enjoinSignUp = () => {
+  isSingUp.value = true;
 }
 </script>
 <style lang="">
